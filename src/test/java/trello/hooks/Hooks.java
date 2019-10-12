cfd8e2200e3ee964ef78e6828ecde987443c79d1
MelissaRoman06 <melissa.roman@fundacion-jala.org>
2019-10-12 09:26:58 -0400

    Screenshot hook added. Needs to be fixed

--- src/test/java/trello/hooks/Hooks.java
+++ src/test/java/trello/hooks/Hooks.java
@@ -12,12 +12,40 @@
 
 package trello.hooks;
 
+import core.selenium.WebDriverManager;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 /**
  * Hooks class.
  *
- * @author Raul Choque
+ * @author Raul Choque, Melissa Román
  * @version 0.0.1
  */
 public class Hooks {
 
+    private WebDriver driver;
+
+    /**
+     * Initializes getting the web driver from web driver manager.
+     */
+    public Hooks() {
+        driver = WebDriverManager.getInstance().getWebDriver();
+    }
+
+    /**
+     * Takes screenshot after the scenario if it has failed.
+     *
+     * @param scenario - Scenario to test.
+     */
+    @After(order = 9999)
+    public void takeScreenshot(final Scenario scenario) {
+        if (scenario.isFailed()) {
+            final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
+    }
 }
